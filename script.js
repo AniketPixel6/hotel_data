581f7012ea6f5e4af55f06d8deb0b09750201ab0
aniket <aniket@pixel6.co>
2020-09-02 12:31:37 +0530

    Add data to map

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// map script
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW5pa2V0cDYiLCJhIjoiY2s4emdvODA5MWlwMjNub2tzdXE1N2I5eiJ9.IrocfCfXkf_E3jEbZety_g';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [12.550343, 55.665957],
+        zoom: 8
+    });
+
+    var marker = new mapboxgl.Marker()
+        .setLngLat([12.550343, 55.665957])
+        .addTo(map);
+
+//  map script end
+
+// data script 
 fetch("hotels.json").then(function(resp){
+    console.log("first");
     return resp.json();
 })
 .then(function(data){
-    console.log(data);
+    console.log("Second");
+    console.log("dta lat", data.latitude);
+    console.log("dta long", data.longitude);
+
 });
